0ee4d4860fc5e54c56f5d4080b46a9e2a1946f88
YuSaki丶Kanade <nep_timeline@outlook.com>
2024-09-09 22:59:40 +0800

    Update AppService.java

--- app/src/main/java/nep/timeline/cirno/services/AppService.java
+++ app/src/main/java/nep/timeline/cirno/services/AppService.java
@@ -8,18 +8,21 @@ import java.util.concurrent.ConcurrentHashMap;
 import nep.timeline.cirno.entity.AppRecord;
 
 public class AppService {
-    private static final Map<Integer, ConcurrentHashMap<String, AppRecord>> APP_RECORD_MAP = new ConcurrentHashMap<>();
+    private static final Map<Integer, Map<String, AppRecord>> APP_RECORD_MAP = new ConcurrentHashMap<>();
 
     public static AppRecord get(String packageName, int userId) {
         if (packageName == null || packageName.equals("android"))
             return null;
 
+        Map<String, AppRecord> appRecords = APP_RECORD_MAP.computeIfAbsent(userId, k -> new ConcurrentHashMap<>());
+
+        if (appRecords.containsKey(packageName))
+            return appRecords.get(packageName);
+
         ApplicationInfo applicationInfo = ActivityManagerService.getApplicationInfo(packageName, userId);
         if (applicationInfo == null)
             return null;
-        
-        APP_RECORD_MAP.computeIfAbsent(userId, k -> new ConcurrentHashMap<>()).put(packageName, new AppRecord());
 
-        return null;
+        return appRecords.put(packageName, new AppRecord(applicationInfo));
     }
 }
