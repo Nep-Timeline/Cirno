4c9fb29cf9952d016aa4c98e5ec1352ffd747986
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2024-09-12 16:02:05 +0800

    Update ApplicationSettings.java

--- app/src/main/java/nep/timeline/cirno/configs/settings/ApplicationSettings.java
+++ app/src/main/java/nep/timeline/cirno/configs/settings/ApplicationSettings.java
@@ -3,5 +3,5 @@ package nep.timeline.cirno.configs.settings;
 import java.util.Set;
 
 public class ApplicationSettings {
-    public Set<String> whiteApps = Set.of("com.tencent.mm");
+    public Set<String> whiteApps = Set.of("com.tencent.mm#0", "com.tencent.mm#999");
 }
